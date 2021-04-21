f8630776fe212642a94ee1c3b998ddff90ed73ab
hvzzzz <hananquispec@outlook.com>
2021-04-21 18:02:51 -0500

    filtering almost done

---
+++ dataprocs/read_data.py
@@ -0,0 +1,18 @@
+import time
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+os.popen('ampy --port /dev/ttyS5 get data.txt d_iir.txt')
+#data = pd.read_csv('d.txt',sep=" ", header = None)
+#data.columns=["Time","Angle","Angle_filt"]
+#time=data['Time']
+#angle=data['Angle']
+#plt.plot(time,angle)
+#plt.savefig("datos.png",dpi=150)
+#print(time)
+#for i in range(len(time)):
+#    print('%5i %5i'% (time[i],angle[i]))
+#data=data.replace("\n"," ")
+#data=np.asarray(data)
+#print(data[0][0:10])
